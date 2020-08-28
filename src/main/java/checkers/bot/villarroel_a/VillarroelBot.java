9cc747ab3cc7db8e93b070bcbb2ddabc1d012980
Andrea Villarroel <anditavillarroel99@gmail.com>
2020-08-28 16:12:36 -0400

    Init branch

---
+++ src/main/java/checkers/bot/villarroel_a/VillarroelBot.java
@@ -0,0 +1,17 @@
+package checkers.bot.villarroel_a;
+
+import checkers.CheckersBoard;
+import checkers.CheckersMove;
+import checkers.CheckersPlayer;
+
+
+public class VillarroelBot implements CheckersPlayer {
+
+    @Override
+    public CheckersMove play(CheckersBoard board) {
+        return null;
+    }
+
+
+
+}
