146b526dba043b480d96a38b2dd639f93098bc11
Andrea Villarroel <anditavillarroel99@gmail.com>
2020-09-07 11:50:39 -0400

    Refactor and Clean Code

--- src/main/java/checkers/bot/villarroel_a/VillarroelBot.java
+++ src/main/java/checkers/bot/villarroel_a/VillarroelBot.java
@@ -9,6 +9,8 @@ import java.util.*;
 
 public class VillarroelBot extends CheckersBoard implements CheckersPlayer {
 
+    Player current_player;
+
     public VillarroelBot() {
         super();
     }
@@ -16,16 +18,16 @@ public class VillarroelBot extends CheckersBoard implements CheckersPlayer {
     @Override
     public CheckersMove play(CheckersBoard board) {
 
-        Player current_player = board.getCurrentPlayer();
+        current_player = board.getCurrentPlayer();
         int depth = 6;
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
 
         CheckersMove best_move = null;
-//        int alpha = Integer.MIN_VALUE;
-//        int beta = Integer.MAX_VALUE;
 
         try {
-            //        Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, player, is_max_state, alpha, beta);
-            Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, current_player, true);
+            Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, current_player, alpha, beta, true);
+
             best_move = Collections.max(utility_list_map.entrySet(), Map.Entry.comparingByValue()).getKey(); // movement with maximum utility
         } catch (BadMoveException e) {
             e.printStackTrace();
@@ -34,57 +36,44 @@ public class VillarroelBot extends CheckersBoard implements CheckersPlayer {
         return best_move;
     }
 
-    private int get_heuristic(CheckersBoard board, Player player) { // The heuristic depends on the number of pieces on the board.
+    private int get_heuristic(CheckersBoard board, Player player) {
 
-//        int number_of_my_plain_pieces = board.countPiecesOfPlayer(player) - getNumberOfQueens(board, player);
-//        int number_of_enemy_plain_pieces = board.countPiecesOfPlayer(otherPlayer(player)) - getNumberOfQueens(board, otherPlayer(player));
-
-        return ((get_number_of_plain_pieces(board, player) * 10)
-                + (get_number_of_queens(board, player) * 100)
-                + (get_number_of_pieces_that_cannot_move(board, otherPlayer(player)) * 5)
-                + (get_number_of_pieces_that_can_move(board, player) * 2)
-                - (get_number_of_plain_pieces(board, otherPlayer(player)) * 10)
-                - (get_number_of_queens(board, otherPlayer(player)) * 100)
-                - (get_number_of_pieces_that_cannot_move(board, player) * 5)
-                - (get_number_of_pieces_that_can_move(board, otherPlayer(player)) * 2)
-
+        return ((get_number_of_plain_pieces(board, player) * 50)
+                + (get_number_of_queens(board, player) * 200)
+                + (get_number_of_pieces_that_cannot_move(board, otherPlayer(player)) * 10)
+                + (get_number_of_pieces_that_can_move(board, player) * 5)
+                + (get_possibility_to_capture(otherPlayer(player)))
+                - (get_number_of_plain_pieces(board, otherPlayer(player)) * 50)
+                - (get_number_of_queens(board, otherPlayer(player)) * 200)
+                - (get_number_of_pieces_that_cannot_move(board, player) * 10)
+                - (get_number_of_pieces_that_can_move(board, otherPlayer(player)) * 5)
         );
 
     }
 
-    //    private int get_utility_value(CheckersBoard board, int depth, Player player, boolean is_max_state, int alpha, int beta) throws BadMoveException {
-        private int get_utility_value(CheckersBoard board, int depth, Player player, boolean is_max_state) throws BadMoveException {
+        private int get_utility_value(CheckersBoard board, int depth, Player player, int alpha, int beta, boolean is_max_state) throws BadMoveException {
 
         if (depth == 0) {
             return get_heuristic(board, player);
         }
 
-        int minimax_value = 0;
-
-//        Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, player, is_max_state, alpha, beta);
-        Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, player, is_max_state);
+        Map<CheckersMove, Integer> utility_list_map = get_move_and_utility_for_successors_list(board, depth, player, alpha, beta, is_max_state);
 
         if (!utility_list_map.isEmpty()) {
             if (is_max_state) {
-                minimax_value = Collections.max(utility_list_map.values());
-//                alpha = Math.max(alpha, value);
-//                if(alpha >= beta){
-//                    minimax_value = beta;
-//                }
+                int value = Collections.max(utility_list_map.values());
+                alpha = Math.max(alpha, value);
+                return alpha;
             } else {
-                minimax_value = Collections.min(utility_list_map.values());
-//                beta = Math.min(alpha, value);
-//                if(beta <= alpha){
-//                    minimax_value = alpha;
-//                }
+                int value = Collections.min(utility_list_map.values());
+                beta = Math.min(beta, value);
+                return beta;
             }
         }
-
-        return minimax_value;
+        return 0;
     }
 
-    public Map<CheckersMove, Integer> get_move_and_utility_for_successors_list(CheckersBoard board, int depth, Player player, boolean is_max_state) throws BadMoveException {
-//    public Map<CheckersMove, Integer> get_move_and_utility_for_successors_list(CheckersBoard board, int depth, Player player, boolean is_max_state, int alpha, int beta) throws BadMoveException {
+    public Map<CheckersMove, Integer> get_move_and_utility_for_successors_list(CheckersBoard board, int depth, Player player, int alpha, int beta, boolean is_max_state) throws BadMoveException {
 
         Map<CheckersBoard, CheckersMove> successors = generate_successors(board);
         Map<CheckersMove, Integer> utility_and_move_list_map = new HashMap<>();
@@ -94,44 +83,45 @@ public class VillarroelBot extends CheckersBoard implements CheckersPlayer {
                 CheckersBoard successor_board = entry.getKey();
                 CheckersMove successor_move = entry.getValue();
                 is_max_state = !is_max_state;
-                utility_and_move_list_map.put(successor_move, get_utility_value(successor_board, depth - 1, otherPlayer(player), is_max_state));
-//                utility_and_move_list_map.put(successor_move, get_utility_value(successor_board, depth - 1, otherPlayer(player), is_max_state, alpha, beta));
+                utility_and_move_list_map.put(successor_move, get_utility_value(successor_board, depth - 1, otherPlayer(player), alpha, beta, is_max_state));
+                is_max_state = !is_max_state;
             }
         }
 
         return utility_and_move_list_map;
     }
 
+
     private Map<CheckersBoard, CheckersMove> generate_successors(CheckersBoard board) throws BadMoveException {
 
-        List<CheckersMove> possible_capture_list = board.possibleCaptures();
-        List<CheckersMove> possible_move_list = board.possibleMoves();
+        List<CheckersMove> move_list = possible_action(board);
 
-//        possible_capture_list.stream().filter(capture -> !isNotMyPiece(capture.getStartRow(), capture.getStartCol())).forEach(possible_capture_list::remove);
-//        possible_move_list.stream().filter(move -> !isNotMyPiece(move.getStartRow(), move.getStartCol())).forEach(possible_move_list::remove);
-//        possible_capture_list.stream().filter(capture -> (ownerOf(capture.getStartRow(), capture.getStartCol())).equals(board.getCurrentPlayer()));
-
-        if (possible_capture_list.isEmpty() && possible_move_list.isEmpty()) {
+        if (move_list.isEmpty()) {
             return null;
         }
 
         Map<CheckersBoard, CheckersMove> successors = new HashMap<>();
 
-        if (!possible_capture_list.isEmpty()) {
-            for (CheckersMove possible_capture : possible_capture_list) {
-                CheckersBoard new_state = board.clone();
-                new_state.processMove(possible_capture);
-                successors.put(new_state, possible_capture);
-            }
-        } else {
-            for (CheckersMove possible_move : possible_move_list) {
-                CheckersBoard new_state = board.clone();
-                new_state.processMove(possible_move);
-                successors.put(new_state, possible_move);
-            }
+        for (CheckersMove possible_move : move_list) {
+            CheckersBoard new_state = board.clone();
+            new_state.processMove(possible_move);
+            successors.put(new_state, possible_move);
         }
 
         return successors;
+    }
+
+
+    public List<CheckersMove> possible_action(CheckersBoard board) {
+
+        List<CheckersMove> possible_capture_list = board.possibleCaptures();
+        List<CheckersMove> possible_move_list = board.possibleMoves();
+
+        if (possible_capture_list.isEmpty()) {
+            return possible_move_list;
+        }
+        return possible_capture_list;
+
     }
 
     public int get_number_of_queens(CheckersBoard board, Player player) {
@@ -157,8 +147,30 @@ public class VillarroelBot extends CheckersBoard implements CheckersPlayer {
         return get_correct_number_of_pieces_after_crowning(board, player) - get_number_of_queens(board, player);
     }
 
+    private int count_pieces_for_list(List<CheckersMove> move_list) {
+        int number_of_pieces = 0;
+        int i = 0;
+        int j = 0;
+
+        for (CheckersMove move : move_list) {
+            if (move.getStartRow() != i || move.getStartCol() != j) {
+                number_of_pieces++;
+                i = move.getStartRow();
+                j = move.getStartCol();
+            }
+        }
+        return number_of_pieces;
+    }
+
     private int get_number_of_pieces_that_can_move(CheckersBoard board, Player player) {
-        return board.possibleCaptures(player).size() + board.possibleMoves(player).size();
+        return count_pieces_for_list(board.possibleMoves(player))+ count_pieces_for_list(board.possibleCaptures(player));
+    }
+
+    private int get_possibility_to_capture(Player player) {
+        if (isCapturePossible(player)) {
+            return -25;
+        }
+        return 25;
     }
 
     private int get_number_of_pieces_that_cannot_move(CheckersBoard board, Player player) {
